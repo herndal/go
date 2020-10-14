0118a03ae92548d5c4c553f54457f6eecf3cd433
hernald <herndonald@gmail.com>
2020-10-14 10:39:39 -0400

    String logic for groups

--- src/scripts/go.js
+++ src/scripts/go.js
@@ -29,9 +29,54 @@ class Board {
   }
 
   validPos(pos) {
-    [x, y] = pos;
+    const [x, y] = pos;
     return (x % this.size === x && y % this.size === y);
   }
 
-  
+  placeStone(color, sPos) {
+    this.grid = this.grid.slice(0, sPos) + color + this.grid.slice(sPos+1);
+  }
+
+  placeManyStones(color, sPoses) {
+    const arr = this.grid.split("");
+    sPoses.forEach(sPos => {
+      arr[sPos] = color;
+    });
+    return this.grid = arr.join("");
+  }
+
+  neighbors(sPos) {
+    [row, col] = this.pos(sPos);
+    const positions = [[row, col-1], [row-1, col], [row, col+1], [row+1, col]];
+    const neighbs = [];
+    positions.forEach(pos => {
+      if (this.validPos(pos)) neighbs.push(this.sPos(pos))
+    })
+    return neighbs;
+  }
+
+  findString(sPos) {
+    const color = this.grid[sPos];
+    const stoneString = [sPos];
+    const bordered = [];
+    const nextInString = [sPos];
+    let currentStone;
+    while (nextInString.length) {
+      currentStone = nextInString.pop();
+      stoneString.push(currentStone);
+      this.neighbors(currentStone).forEach(neighb => {
+        if (this.grid[neighb] === color && !stoneString.includes(neighb)) {
+          nextInString.push(neighb);
+        }
+        if (this.grid[neighb] !== color) {
+          bordered.push(neighb);
+        }
+      });
+    }
+    return ({
+      stoneString,
+      bordered
+    });
+  }
+
 }
