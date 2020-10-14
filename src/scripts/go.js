645cf2e19d2862dddef71713afd5b306b022f0fb
hernald <herndonald@gmail.com>
2020-10-14 15:14:16 -0400

    Score board at any given point

--- src/scripts/go.js
+++ src/scripts/go.js
@@ -1,4 +1,4 @@
-const [white, black, empty] = ["X", "O", "_"];
+const [white, black, empty, neutral] = ["X", "O", "_", "."];
 
 class Board {
   constructor(size) {
@@ -104,36 +104,62 @@ class Board {
     const sPos = this.sPos(pos);
     const opponent = swap(color);
     const captured = 0;
-
     //do nothing if position is occupied
     if (this.grid[sPos] !== empty) return;
-
     this.placeStone(color, sPos);
-
     //find opponent stones in neighbors, capture if possible
     this.neighbors(sPos).forEach(neighb => {
       if (this.grid[neighb] === opponent) {
         captured += this.captureSurrounded(neighb).length;
       }
     });
-
     //check for suicidal move
-    while (oppStones.length) {
-      
+    if (this.captureSurrounded(sPos).length) {
+      throw "Suicide! Try again";
     }
-
-
-
+    //check for ko
     if (this.prevPositions.includes(this.grid)) {
       this.grid = this.prevPositions[this.prevPositions.length - 1];
       throw "Ko! Invalid move, try again";
     }
+    player.captured += captured;
     this.prevPositions.push(this.grid);
     return this.grid;
   }
 
+  score() {
+    //fill grid territories (lowercase letters for visual difference)
+    while (this.grid.includes(empty)) {
+      let idx = this.grid.indexOf(empty);
+      let { stoneString, bordered } = this.findString(idx);
+      let borderColors = bordered.map(sPos => {
+        return this.grid[sPos].toUpperCase;
+      });
+      let matched = true;
+      let firstColor = borderColors[0];
+      borderColors.forEach(color => {
+        if (color !== firstColor) matched = false;
+      });
+      if (matched) {
+        this.placeManyStones(firstColor.toLowerCase(), stoneString);
+      } else {
+        this.placeManyStones(neutral, stoneString);
+      }
+    }
+    //count up the final scores
+    const finalGrid = this.grid.toUpperCase();
+    let blackScore = 0;
+    let whiteScore = 0;
+    for (let i = 0; i < finalGrid.length; i++) {
+      if (finalGrid[i] === black) blackScore++;
+      if (finalGrid[i] === white) whiteScore++;
+    };
+    return ({
+      blackScore,
+      whiteScore
+    })
+  }
 }
-
 
 class Player {
   constructor(color) {
