919f67117d12ef44f697e5bcc4179cdce030d356
hernald <herndonald@gmail.com>
2020-12-02 16:59:52 -0500

    Add about links to main nav menu

--- src/scripts/dropdowns.js
+++ src/scripts/dropdowns.js
@@ -1,3 +1,5 @@
+//Author and About dropdowns
+
 export function dropdown(parent, name, listElements) {
   const dropdown = document.createElement('ul');
   const button = document.createElement('button');
@@ -15,7 +17,6 @@ export function dropdown(parent, name, listElements) {
   button.onclick = reveal(dropdown, list);
   document.onclick = hideLists;
 }
-
 function reveal(dropdown, list) {
   return event => {
     event.stopPropagation();
@@ -23,7 +24,6 @@ function reveal(dropdown, list) {
     list.classList.toggle('show');
   }
 }
-
 function hideLists() {
   const lists = document.querySelectorAll('.list');
   for(let i = 0; i < lists.length; i++) {
@@ -32,7 +32,6 @@ function hideLists() {
     }
   }
 }
-
 export function authorElements() {
   const name = document.createElement('h3');
   name.classList.add('red');
@@ -41,40 +40,46 @@ export function authorElements() {
   links.id = 'author-links';
   const github = listLink(
     'https://github.com/herndal/go',
-    '<i class="fab fa-github-square"></i>'
+    '<i class="fab fa-github-square"></i>',
+    'My Github repository for this project'
   );
   const linkedIn= listLink(
     'https://www.linkedin.com/in/donald-herndon-8364b01a5/',
-    '<i class="fab fa-linkedin"></i>'
+    '<i class="fab fa-linkedin"></i>',
+    'My LinkedIn'
   );
   const twitter = listLink(
     'https://twitter.com/donald_builds',
-    '<i class="fab fa-twitter-square"></i>'
+    '<i class="fab fa-twitter-square"></i>',
+    'My Twitter'
   );
   links.append(github, linkedIn, twitter);
   return [name, links];
 }
-
 export function aboutElements() {
   const wikipedia = listLink(
-    'https://en.wikipedia.org/wiki/Go_(game)',
+    'https://en.wikipedia.org/wiki/History_of_Go',
     '<i class="fab fa-wikipedia-w"></i>',
+    'Wikipedia: the History of Go',
     'wikipedia'
   );
-  return [wikipedia];
+  const aga = listLink(
+    'https://www.usgo.org/',
+    img('https://www.usgo.org/theme/images/agalogo2.png'),
+    'The American Go Association. Founded in New York City in 1935, AGA is one of the oldest Western Go associations.',
+    'aga'
+  );
+  const deepMind = listLink(
+    'https://deepmind.com/research/case-studies/alphago-the-story-so-far',
+    img('https://upload.wikimedia.org/wikipedia/commons/1/12/DeepMind_logo.png'),
+    'AlphaGo: The project at Google\'s DeepMind that created the first program capable of defeating a Go world champion in 2016. Their story inspired this project!',
+    'deepMind'
+  )
+  return [aga, wikipedia, deepMind];
 }
 
-function listLink(href, html, ...classes) {
-  const li = document.createElement('li');
-  const link = document.createElement('a');
-  li.classList.add('link');
-  link.classList.add(...classes);
-  link.href = href;
-  link.innerHTML = html;
-  link.target = 'blank';
-  li.appendChild(link);
-  return li;
-}
+
+//In-game instructions modal
 
 export function instructions(game) {
   const root = document.getElementById('root');
@@ -89,7 +94,6 @@ export function instructions(game) {
   button.onclick = showInstructions(instructions);
   document.onclick = hideInstructions(instructions);
 }
-
 function fillInstructions(instructions) {
   const gettingStarted = document.createElement('div');
   const gettingStartedTitle = document.createElement('h4');
@@ -130,14 +134,12 @@ function fillInstructions(instructions) {
   winning.append(winningTitle, winningInfo)
   instructions.append(gettingStarted, gamePlay, winning);
 }
-
 function showInstructions(instructions) {
   return event => {
     event.stopPropagation();
     instructions.classList.toggle('show');
   }
 }
-
 function hideInstructions(instructions) {
   return event => {
     if (instructions.classList.contains('show')) {
@@ -146,14 +148,57 @@ function hideInstructions(instructions) {
   }
 }
 
+//Utility functions for creating lists
+
+function listLink(href, html, tip, ...classes) {
+  const li = document.createElement('li');
+  const link = document.createElement('a');
+  li.classList.add('link');
+  link.classList.add(...classes);
+  link.href = href;
+  if(typeof html === 'string') {
+    link.innerHTML = html;
+  } else {
+    link.append(html);
+  }
+  tooltip(link, tip);
+  link.target = 'blank';
+  li.appendChild(link);
+  return li;
+}
 function li(text) {
   const element = document.createElement('li');
   element.innerHTML = text;
   return element;
 }
-
 function img(url) {
   const element = document.createElement('img');
   element.src = url;
   return element;
+}
+function tooltip(link, tip) {
+  link.title = tip;
+  link.addEventListener('mouseover', showTip);
+  link.addEventListener('mouseout', hideTip);
+}
+function showTip() {
+  const title = this.title;
+  this.title = '';
+  this.setAttribute('tooltip', title);
+  const tooltip = document.createElement('div');
+  tooltip.classList.add('tooltip');
+  tooltip.append(title);
+  document.body.firstChild.before(tooltip);
+  tooltip.setAttribute('style', 'max-width: 13em');
+  const linkProps = this.getBoundingClientRect();
+  const tooltipProps = tooltip.getBoundingClientRect(); 
+  const top = linkProps.top + (linkProps.height + 15);
+  const left = linkProps.left + (linkProps.width / 2) - (tooltipProps.width / 2);
+  tooltip.setAttribute('style', `max-width: 13em; top: ${top}px; left: ${left}px;`);
+}
+function hideTip() {
+  const title = this.getAttribute('tooltip');
+  this.title = title;
+  this.removeAttribute('tooltip');
+  document.querySelector(".tooltip").remove();
 }
